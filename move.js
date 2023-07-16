50e172b6d9886551530960903d6adce7ccf229f9
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-07-16 12:03:39 -0500

    Done

--- move.js
+++ move.js
@@ -1,12 +1,158 @@
 function move(element) {
-    element.style.position = 'fixed'
-
+    element.style.position = 'fixed';
+  
     function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+      element.style.left = left + 'px';
+      element.style.bottom = bottom + 'px';
     }
-
+  
+    function moveWithArrowKeys(left, bottom, handleDirectionChange) {
+      let direction = null;
+      let x = left;
+      let y = bottom;
+  
+      element.style.left = x + 'px';
+      element.style.bottom = y + 'px';
+  
+      function moveCharacter() {
+        if (direction === 'west' && x > 0) {
+          x -= 1;
+        }
+        if (direction === 'north' && y < window.innerHeight - element.offsetWidth) {
+          y += 1;
+        }
+        if (direction === 'east' && x < window.innerWidth - element.offsetWidth) {
+          x += 1;
+        }
+        if (direction === 'south' && y > 0) {
+          y -= 1;
+        }
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
+      }
+  
+      setInterval(moveCharacter, 1);
+  
+      document.addEventListener('keydown', function (e) {
+        if (e.repeat) return;
+  
+        if (e.key === 'ArrowLeft') {
+          direction = 'west';
+        }
+        if (e.key === 'ArrowUp') {
+          direction = 'north';
+        }
+        if (e.key === 'ArrowRight') {
+          direction = 'east';
+        }
+        if (e.key === 'ArrowDown') {
+          direction = 'south';
+        }
+        
+        if (handleDirectionChange && typeof handleDirectionChange === 'function') {
+          handleDirectionChange(direction);
+        }
+      });
+  
+      document.addEventListener('keyup', function (e) {
+        direction = null;
+        
+        if (handleDirectionChange && typeof handleDirectionChange === 'function') {
+          handleDirectionChange(direction);
+        }
+      });
+    }
+  
     return {
-        to: moveToCoordinates
-    }
-}
+      to: moveToCoordinates,
+      withArrowKeys: moveWithArrowKeys
+    };
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD CODE
+
+// function move(element) {
+//     element.style.position = 'fixed'
+
+//     function moveToCoordinates(left, bottom) {
+//         element.style.left = left + 'px'
+//         element.style.bottom = bottom + 'px'
+//     }
+
+//     function moveWithArrowKeys(left, bottom, handleDirectionChange){
+//         let direction = null;
+//         let x = left;
+//         let y = bottom;
+    
+//         element.style.left = x + 'px'
+//         element.style.bottom = y + 'px'
+        
+//         function moveCharacter(){ 
+//             if(direction === 'west'){
+//                 x-=1
+//             }
+//             if(direction === 'north'){
+//                 y+=1
+//             }
+//             if(direction === 'east'){
+//                 x+=1
+//             }
+//             if(direction === 'south'){
+//                 y-=1
+//             }
+//             element.style.left = x + 'px'
+//             element.style.bottom = y + 'px'
+//         }
+        
+//         setInterval(moveCharacter, 1)
+        
+//         document.addEventListener('keydown', function(e){
+//             if(e.repeat) return;
+        
+//             if(e.key === 'ArrowLeft'){
+//                 direction = 'west'
+//             }
+//             if(e.key === 'ArrowUp'){
+//                 direction = 'north'
+//             }
+//             if(e.key === 'ArrowRight'){
+//                 direction = 'east'
+//             }
+//             if(e.key === 'ArrowDown'){
+//                 direction = 'south'
+//             }
+//             handleDirectionChange(direction)
+//         })
+        
+//         document.addEventListener('keyup', function(e){
+//             direction = null
+//             handleDirectionChange(direction)
+//         })
+//     }
+    
+
+//     return {
+//         to: moveToCoordinates,
+//         withArrowKeys: moveWithArrowKeys
+//     }        
+// }
